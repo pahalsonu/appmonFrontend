cf2d4ffcc340cf737f167d4ddafbdfaab6c02283
Mansoor <ali.syedmansoor1997@gmail.com>
2021-01-25 22:27:00 +0530

    Created AuthLayout , and Added login Component to it via props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import AuthLayout from "./layouts/AuthLayout";
+
+import Login from "./views/Auth/Login";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "./assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/css/argon-dashboard-react.css";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <AuthLayout component={Login} />
+    </Router>
+  );
 };
 
 export default App;
