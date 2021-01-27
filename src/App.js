9e04f401f56dccf936e34f1130abd40ef3c99222
pahalsonu <pahalsonu@gmail.com>
2021-01-27 13:19:44 +0530

    merge with mansoor changes but component is not solved

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
-import AuthNavbar from "./components/Navbars/AuthNavbar"
-import AuthFooter from "./components/Footers/AuthFooter"
-import { BrowserRouter as Router } from 'react-router-dom'
+import AuthLayout from "./layouts/AuthLayout";
 
-import "./assets/plugins/nucleo/css/nucleo.css"
-import "@fortawesome/fontawesome-free/css/all.min.css"
+import Login from "./views/Auth/Login";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "./assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/css/argon-dashboard-react.css";
 
 const App = () => {
-  return( // eslint-disable-next-line
-  <Router>
-    <div style={{ backgroundColor: "darkblue" }}>
-      <AuthNavbar />
-      <AuthFooter />
-    </div>
-  </Router>
-)
+  return (
+    <Router>
+      <AuthLayout component={Login} />
+    </Router>
+  );
 };
 
 export default App;
