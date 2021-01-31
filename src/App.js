23ec0a621b0ed2f5cf2ec471fbceafc337721063
pahalsonu <pahalsonu@gmail.com>
2021-01-31 21:26:41 +0530

    react hello worlf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import AuthLayout from "./layouts/AuthLayout";
 
-import Login from "./views/Auth/Login";
-
-import { BrowserRouter as Router } from "react-router-dom";
-
-import "./assets/plugins/nucleo/css/nucleo.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./assets/css/argon-dashboard-react.css";
-
-// User Comp
-import UserNavbar from "./components/Navbars/UserNavbar";
-import UserLayout from "./layouts/UserLayout";
-
+import {BrowserRouter as Router} from "react-router-dom";
 const App = () => {
   return (
     <Router>
-      <div style={{ backgroundColor: "darkblue" }}>
-        <UserLayout />
+      <div >
+       Hello World
       </div>
     </Router>
   );
