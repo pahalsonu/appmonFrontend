1b4e9fc054a2b229a4c637ae88d2778ae3409141
pahalsonu <pahalsonu@gmail.com>
2021-02-01 13:11:53 +0530

    UserNavbar completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-import {BrowserRouter as Router} from "react-router-dom";
+//import css
+import "./assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/css/argon-dashboard-react.css";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import UserNavbar from "./component/Navbars/UserNavbar"
 const App = () => {
   return (
+
+    <>
     <Router>
-      <div >
-       Hello World
-      </div>
-    </Router>
+      <UserNavbar />
+      </Router>
+    </>
+
   );
 };
 
