503b95e5e2ba919a5186a9f2f0aa2b92ddf7b79a
pahalsonu <pahalsonu@gmail.com>
2021-02-06 21:28:02 +0530

    auth navbar completed but aqua bg color is given to understand

--- src/components/Navbars/AuthNavbars.js
+++ src/components/Navbars/AuthNavbars.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import {
     Navbar,
-    Collapse,
+    UncontrolledCollapse,
     Nav,
     NavItem,
     NavbarBrand,
     Container,
-    UncontrolledCollapse
+    NavLink,
+    DropdownItem
 } from "reactstrap"
 
 
@@ -15,18 +16,108 @@ export class AuthNavbars extends Component {
     render() {
         return (
             <>
-                <Navbar expand="md" className="navbar-top navbar-horizontal navbar-dark">
+                <Navbar expand="md" className="navbar-top bg-info navbar-horizontal navbar-dark">
                     <Container className="px-4">
-                        <NavbarBrand  tag={Link} to="/">
+                        <NavbarBrand tag={Link} to="/">
                             <span className="nav-link navbar-light navbar-text text-white bold">
                                 Appmon
                             </span>
                         </NavbarBrand>
                         <button className="navbar-toggler" id="navbar-collapse-main">
-                            <span className="navbar-toggler-icon"/>
+                            <span className="navbar-toggler-icon" />
                         </button>
                         <UncontrolledCollapse navbar toggler="#navbar-collapse-main">
                             <Nav navbar className="navbar-collapse-header d-md-none" >
+                                <NavItem>
+                                    <NavLink href="/" xs="6">
+                                        Appmon
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem className="collapse-close" xs="6">
+                                    <button
+                                        className="navbar-toggler"
+                                        id="navbar-collapse-main"
+                                    >
+                                        <span />
+                                        <span />
+                                    </button>
+                                </NavItem>
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/register" className="nav-link-icon">
+                                        <span className="nav-link-inner--text">Register</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/contactus" className="nav-link-icon">
+                                        <span className="nav-link-inner--text">Contact Us</span>
+                                    </NavLink>
+                                </NavItem>
+                                <DropdownItem divider className="d-lg-none" />
+                                <NavItem>
+                                    <NavLink
+                                        href="https://www.facebook.com/appmon"
+                                        className="nav-link-icon"
+                                    >
+                                        <NavItem>
+                                            <i className="fab fa-facebook-square"></i>
+                                            <span className="nav-link-inner--text d-lg-none">
+                                                Facebook
+                                            </span>
+                                        </NavItem>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink
+                                        href="https://www.instagram.com/appmon"
+                                        className="nav-link-icon"
+                                    >
+                                        <NavItem>
+                                            <i className="fab fa-instagram"></i>
+                                            <span className="nav-link-inner--text d-lg-none">
+                                                Instagram
+                      </span>
+                                        </NavItem>
+                                    </NavLink>
+                                </NavItem>
+
+                                <NavItem>
+                                    <NavLink
+                                        href="https://www.twitter.com/appmon"
+                                        className="nav-link-icon"
+                                    >
+                                        <NavItem>
+                                            <i className="fab fa-twitter-square"></i>
+                                            <span className="nav-link-inner--text d-lg-none">
+                                                Twitter
+                                             </span>
+                                        </NavItem>
+                                    </NavLink>
+                                </NavItem>
+
+                                <NavItem>
+                                    <NavLink
+                                        href="https://www.github.com/appmon"
+                                        className="nav-link-icon"
+                                    >
+                                        <NavItem>
+                                            <i className="fab fa-github"></i>
+                                            <span className="nav-link-inner--text d-md-none d-lg-none">
+                                                Github
+                                            </span>
+                                        </NavItem>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/register" className="nav-link-icon">
+                                        <NavItem className="d-none d-lg-block">
+                                            <i className="fas fa-sign mr-2"></i>
+                                            <span className="nav-link-inner--text">Sign Up</span>
+                                        </NavItem>
+                                    </NavLink>
+                                </NavItem>
+
 
                             </Nav>
                         </UncontrolledCollapse>
